2b283d1927daa3ddd1069d227c60ddef57d37298
YooInHak <dlsgkr2856@gmail.com>
2025-11-06 16:22:18 +0900

    [test] git branch 연결 테스트

---
+++ src/libs/structs.js
@@ -0,0 +1,5 @@
+import * as s from "superstruct";
+
+export const CreateGroupLike = s.object({
+    
+});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PORT } from './libs/constants.js';
 import cors from 'cors';
+import grouprouter from './routers/grouprouters.js';
 
 const app = express();
 app.use(cors());
@@ -8,6 +9,8 @@ app.use(express.json());
 
 
 
+
+app.use('/groups', grouprouter);
 
 app.listen(PORT, () => {
     console.log("Server is Online");
